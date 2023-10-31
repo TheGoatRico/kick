3f051bb5b9c4830eb9b89e69ce0a89204b1d95b9
Will Holmes <willholmes97@icloud.com>
2023-10-31 18:47:11 +0000

    Rough guidelines for layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,26 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`font-sans ${inter.variable}`}>{children}</body>
+      <body className={`font-sans flex flex-col ${inter.variable} overflow-hidden`}>
+        <nav className="sticky flex h-[60px] w-full items-center justify-center text-center z-[100] top-0 bg-secondary-lighter">
+          <div className="flex h-full w-fit items-center md:pr-6"> <div className="p-6">Kick</div></div>
+          <div className="relative flex w-full items-center justify-end gap-3 px-2 md:justify-center lg:px-3"></div>
+          <div className="flex w-full items-center justify-end px-2 md:w-fit lg:px-3"></div>
+        </nav>
+        <main className="flex flex-row">
+          <div className="relative bg-secondary w-64 px-3.5 py-5">
+            <ul>
+              <li>test</li>
+              <li>test</li>
+              <li>test</li>
+              <li>test</li>
+            </ul>
+          </div>
+          <div className="relative h-full w-full grow border-l border-secondary-lighter bg-gray-900 overflow-y-auto">
+            {children}
+          </div>
+        </main>
+      </body>
     </html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,10 @@ import { fontFamily } from "tailwindcss/defaultTheme";
 export default {
   content: ["./src/**/*.tsx"],
   theme: {
+    colors: {
+      secondary: "rgb(25 27 31)",
+      "secondary-lighter": "rgb(36 39 44)",
+    },
     extend: {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
