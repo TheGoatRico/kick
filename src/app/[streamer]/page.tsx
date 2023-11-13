943ef57e71108fc86697404e9d90418c218cb1cd
Will Holmes <willholmes97@icloud.com>
2023-11-13 18:47:11 +0000

    Created rough outline for streamer page

---
+++ src/app/[streamer]/page.tsx
@@ -0,0 +1,10 @@
+export default function LiveStreamPage() {
+    return (
+        <div className="w-full h-screen flex flex-col">
+            <div className="grid grid-cols-4">
+                <div className="col-span-3">Streamer</div>
+                <div className="col-span-1">Chat</div>
+            </div>
+        </div>
+    )
+}
